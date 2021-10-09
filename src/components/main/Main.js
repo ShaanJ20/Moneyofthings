7e25edc01e237e4e16cf6b882faab1e0421361de
ShaanJ20 <shaan.johari18@gmail.com>
2021-10-09 20:18:15 +0530

    Made the whole site

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,15 +1,6 @@
 import React, {useState, useEffect} from 'react'
 import './main.css'
-
-import elonmusk from './elonmusk.jpg'
-import jeffbezos from './jeff.jpg'
-import Bernard from './bernard.jpg'
-import billgates from './billgates.jpg'
-import mark from './mark.jpg'
-import ellison from './ellison.jpg'
-import page from './page.jpg'
-import sergey  from './sergey.jpg'
-import warren from './warren.jpg'
+import {iphones,burgers,books,cars,university,games,yt,Netflix,Google,pens,rubix,ps} from './index'
 
 export default function Main() {
 
@@ -27,59 +18,161 @@ export default function Main() {
 
   //-----------------------------------------------------------------------------------------//
 
-  // Elon musk
-  const [elonmuskmoney, setelonmuskmoney] = useState(0)
+  // Iphone
+  const [iphonesmoney, setiphonesmoney] = useState(0)
 
-  function handlelon(){
-    setelonmuskmoney(elonmuskmoney+17);
+  function handleiphone(){
+    setiphonesmoney(iphonesmoney+(43960));
   }
 
   useEffect(() => {
-    setTimeout(handlelon, 250);
-  }, [elonmuskmoney])
+    setTimeout(handleiphone, 250);
+  }, [iphonesmoney])
 
   //-----------------------------------------------------------------------------------------//
 
-  // jeff bezos
-  const [jeffbezosmoney, setjeffbezosmoney] = useState(0)
+  // Burgers
+  const [burgermoney, setburgermoney] = useState(0)
 
-  function handljeff(){
-    setjeffbezosmoney(jeffbezosmoney+17);
+  function handleburger(){
+    setburgermoney(burgermoney+(243));
   }
 
   useEffect(() => {
-    setTimeout(handljeff, 250);
-  }, [jeffbezosmoney])
-
+    setTimeout(handleburger, 250);
+  }, [burgermoney])  
 
   //-----------------------------------------------------------------------------------------//
-  
-  // bernard arnault
-  const [Bernardmoney, setBernardmoney] = useState(0)
 
-  function handlBernard(){
-    setBernardmoney(Bernardmoney+17);
+  // books
+  const [booksmoney, setbooksmoney] = useState(0)
+
+  function handlebooks(){
+    setbooksmoney(booksmoney+(21));
   }
 
   useEffect(() => {
-    setTimeout(handlBernard, 250);
-  }, [Bernardmoney])
-
+    setTimeout(handlebooks, 250);
+  }, [booksmoney])
 
   //-----------------------------------------------------------------------------------------//
-  
-  // bill gates
-  const [billgatesmoney, setbillgatesmoney] = useState(0)
 
-  function handlbillgates(){
-    setbillgatesmoney(billgatesmoney+17);
+  // cars
+  const [carsmoney, setcarsmoney] = useState(0)
+
+  function handlecars(){
+    setcarsmoney(carsmoney+(6587));
   }
 
   useEffect(() => {
-    setTimeout(handlbillgates, 250);
-  }, [billgatesmoney])
+    setTimeout(handlecars, 250);
+  }, [carsmoney])
 
+  //-----------------------------------------------------------------------------------------//
 
+  // university
+  const [universitymoney, setuniversitymoney] = useState(0)
+
+  function handleuniversity(){
+    setuniversitymoney(universitymoney+(12683));
+  }
+
+  useEffect(() => {
+    setTimeout(handleuniversity, 250);
+  }, [universitymoney])
+
+  //-----------------------------------------------------------------------------------------//
+
+  // Games
+  const [gamesmoney, setgamesmoney] = useState(0)
+
+  function handlegames(){
+    setgamesmoney(gamesmoney+(16996));
+  }
+
+  useEffect(() => {
+    setTimeout(handlegames, 250);
+  }, [gamesmoney])
+
+  //-----------------------------------------------------------------------------------------//
+
+  // Youtube
+  const [youtubemoney, setyoutubemoney] = useState(0)
+
+  function handleyoutube(){
+    setyoutubemoney(youtubemoney+(2498));
+  }
+
+  useEffect(() => {
+    setTimeout(handleyoutube, 250);
+  }, [youtubemoney])
+
+  //-----------------------------------------------------------------------------------------//
+
+  // Netflix
+  const [Netflixmoney, setNetflixmoney] = useState(0)
+
+  function handleNetflix(){
+    setNetflixmoney(Netflixmoney+(3170));
+  }
+
+  useEffect(() => {
+    setTimeout(handleNetflix, 250);
+  }, [Netflixmoney])
+
+  //-----------------------------------------------------------------------------------------//
+
+  // Google
+  const [Googlemoney, setGooglemoney] = useState(0)
+
+  function handleGoogle(){
+    setGooglemoney(Googlemoney+(3170));
+  }
+
+  useEffect(() => {
+    setTimeout(handleGoogle, 250);
+  }, [Googlemoney])
+
+  //-----------------------------------------------------------------------------------------//
+
+  // Pens
+  const [pensmoney, setpensmoney] = useState(0)
+
+  function handlepens(){
+    setpensmoney(pensmoney+(253));
+  }
+
+  useEffect(() => {
+    setTimeout(handlepens, 250);
+  }, [pensmoney])
+
+  //-----------------------------------------------------------------------------------------//
+
+  // rubix
+  const [rubixmoney, setrubixmoney] = useState(0)
+
+  function handlerubix(){
+    setrubixmoney(rubixmoney+(2));
+  }
+
+  useEffect(() => {
+    setTimeout(handlerubix, 250);
+  }, [rubixmoney])
+
+  //-----------------------------------------------------------------------------------------//
+
+  // ps
+  const [psmoney, setpsmoney] = useState(0)
+
+  function handleps(){
+    setpsmoney(psmoney+(4414));
+  }
+
+  useEffect(() => {
+    setTimeout(handleps, 250);
+  }, [psmoney])
+
+  //-----------------------------------------------------------------------------------------//
 
     return (
         <div>
@@ -89,51 +182,73 @@ export default function Main() {
   <div className="second-counter">
         <p className='header-title'>{counter} seconds has Passed and they made</p>
   </div>
+  <p className='madewithlove'>Made with ❤️ by <a href='https://twitter.com/shaanforlife' target='_blank'>Shaan</a></p>
+  <p className='contribution'>Hey If you would love to add on some more amazing things on this website <a>Start contributing</a> </p>
 
   <div className="boxes-parent">
+
     <div className="boxes">
-      <h2>Elon Musk - $<a style={{color:'#279700'}} >{elonmuskmoney}</a></h2>
-      <img src={elonmusk} alt="Elon Musk" />
+      <h2>iPhones - $<a style={{color:'#279700'}} >{iphonesmoney}</a></h2>
+      <img src={iphones} alt="iphones" />
     </div>
 
     <div className="boxes">
-      <h2>Jeff Bezos -  $<a style={{color:'#279700'}} >{jeffbezosmoney}</a></h2>
-      <img src={jeffbezos} alt="Jeff bezos" />
+      <h2>Burgers - $<a style={{color:'#279700'}} >{burgermoney}</a></h2>
+      <img src={burgers} alt="burgers" />
     </div>
 
     <div className="boxes">
-      <h2>Bernard Arnault -  $<a style={{color:'#279700'}} >{Bernardmoney}</a></h2>
-      <img src={Bernard} alt="Bernard" />
+      <h2>Books - $<a style={{color:'#279700'}} >{booksmoney}</a></h2>
+      <img src={books} alt="books" />
     </div>
 
     <div className="boxes">
-      <h2>Bill Gates -  $<a style={{color:'#279700'}} >{billgatesmoney}</a></h2>
-      <img src={billgates} alt="Bill gates" />
+      <h2>Cars - $<a style={{color:'#279700'}} >{carsmoney}</a></h2>
+      <img src={cars} alt="cars" />
     </div>
+
     <div className="boxes">
-      <h2>Mark Zuckerberg -  $<a style={{color:'#279700'}} >1231232</a></h2>
-      <img src={mark} alt="Mark" />
+      <h2>University - $<a style={{color:'#279700'}} >{universitymoney}</a></h2>
+      <img src={university} alt="university" />
     </div>
+
     <div className="boxes">
-      <h2>Larry Ellison -  $<a style={{color:'#279700'}} >1231232</a></h2>
-      <img src={ellison} alt="ellison" />
+      <h2>Games - $<a style={{color:'#279700'}} >{gamesmoney}</a></h2> 
+      <img src={games} alt="games" />
     </div>
+
     <div className="boxes">
-      <h2>Larry Page -  $<a style={{color:'#279700'}} >1231232</a></h2>
-      <img src={page} alt="page" />
+      <h2>Youtube - $<a style={{color:'#279700'}} >{youtubemoney}</a></h2>
+      <img src={yt} alt="yt" />
     </div>
+
     <div className="boxes">
-      <h2>Sergey Brin -  $<a style={{color:'#279700'}} >1231232</a></h2>
-      <img src={sergey} alt="sergey" />
+      <h2>Netflix - $<a style={{color:'#279700'}} >{Netflixmoney}</a></h2>
+      <img src={Netflix} alt="Netflix" />
     </div>
+
     <div className="boxes">
-      <h2>Warren Buffett -  $<a style={{color:'#279700'}} >1231232</a></h2>
-      <img src={warren} alt="warren" />
+      <h2>Google - $<a style={{color:'#279700'}} >{Googlemoney}</a></h2>
+      <img src={Google} alt="Google" />
     </div>
+
+    <div className="boxes">
+      <h2>Pens - $<a style={{color:'#279700'}} >{pensmoney}</a></h2>
+      <img src={pens} alt="pens" />
+    </div>
+
+    <div className="boxes">
+      <h2>Rubix - $<a style={{color:'#279700'}} >{rubixmoney}</a></h2>
+      <img src={rubix} alt="rubix" />
+    </div>
+
+    <div className="boxes">
+      <h2>Playstations - $<a style={{color:'#279700'}} >{psmoney}</a></h2>
+      <img src={ps} alt="ps" />
+    </div>
+
   </div>
 </div>
-
-            
         </div>
     )
 }
---
+++ src/components/main/index.js
@@ -0,0 +1,27 @@
+import iphones from '../main/assets/iphones.jpg'
+import burgers from '../main/assets/burgers.jpg'
+import books from '../main/assets/books.jpg'
+import cars from '../main/assets/cars.jpg'
+import university from '../main/assets/university.jpg'
+import games from '../main/assets/games.jpg'
+import yt from '../main/assets/yt.png'
+import Netflix from '../main/assets/Netflix.png'
+import Google from '../main/assets/Google.png'
+import pens from '../main/assets/pens.jpg'
+import rubix from '../main/assets/rubix.jpg'
+import ps from '../main/assets/ps.jpg'
+
+export{
+    iphones,
+    burgers,
+    books,
+    cars,
+    university,
+    games,
+    yt,
+    Netflix,
+    Google,
+    pens,
+    rubix,
+    ps
+}
